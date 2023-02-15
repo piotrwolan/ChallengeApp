4277cf4aa4bdffcd18965b9a316fe9cae8cb4751
piotrwolan <piotr.wolan@gmail.com>
2023-02-16 00:15:39 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool isFemale = true;
+int age = 61;
+
+if (isFemale == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+if (isFemale == false && age < 18)
+{ 
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+if (isFemale == true && age >=60)
+{
+    Console.WriteLine("Kobieta osiągnęła wiek emerytalny");
+}
