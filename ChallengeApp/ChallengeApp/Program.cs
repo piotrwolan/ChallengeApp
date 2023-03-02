22d47629c14ff579af3c20f753128aaccc9322ba
piotrwolan <piotr.wolan@gmail.com>
2023-03-03 00:12:45 +0100

    zadanie dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,69 @@
-﻿string name = "Ewa";
-bool isFemale = true;
-int age = 61;
+﻿int number = 5443;
+string numberAsString = number.ToString ();
+char[] letters = numberAsString.ToArray ();
 
-if (isFemale == true && age < 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-
-if (name == "Ewa" && age == 33)
+    if(letter == '0')
     {
-        Console.WriteLine("Ewa, lat 33");
+        counter0++;
     }
-
-if (isFemale == false && age < 18)
-{ 
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
 
-if (isFemale == true && age >=60)
-{
-    Console.WriteLine("Kobieta osiągnęła wiek emerytalny");
-}
+Console.WriteLine(counter0);
+Console.WriteLine(counter1);
+Console.WriteLine(counter2);
+Console.WriteLine(counter3);
+Console.WriteLine(counter4);
+Console.WriteLine(counter5);
+Console.WriteLine(counter6);
+Console.WriteLine(counter7);
+Console.WriteLine(counter8);
+Console.WriteLine(counter9);
